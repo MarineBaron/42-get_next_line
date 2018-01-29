00f678fcc3dd4d7f85a2000ee91fc6c4678bbf82
Marine BARON <mbaron@student.42.fr>
2018-01-29 11:56:08 +0100

    KO on test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mbaron <mbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 18:19:50 by mbaron            #+#    #+#             */
-/*   Updated: 2018/01/27 12:28:04 by mbaron           ###   ########.fr       */
+/*   Updated: 2018/01/29 10:56:20 by mbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,33 @@ static int	get_endl(char *str)
 	return (i);
 }
 
-static void	get_new_buf(char **str, char buf[], int res)
+static char	*get_new_buf(char *str, char buf[], int res)
 {
 	char	*tmp;
 
-	tmp = *str;
-	*str = ft_strnew(ft_strlen(*str) + res + 1);
-	*str[0] = '\0';
-	ft_strcpy(*str, tmp);
-	ft_strncat(*str, buf, res);
+	ft_putendl("--- new buf");
+	ft_putendl("str : ");
+	ft_putendl(str);
+	tmp = str;
+	ft_putendl("tmp : ");
+	ft_putendl(tmp);
+	ft_putendl("buf: ");
+	ft_putendl(buf);
+	ft_putstr("res: ");
+	ft_putnbr(res);
+	ft_putendl("");
+	str = ft_strnew(ft_strlen(tmp) + res + 1);
+	//*str[0] = '\0';
+	ft_putendl("str + 0 : ");
+	ft_putendl(str);
+	ft_strcpy(str, tmp);
+	ft_putendl("str + tmp : ");
+	ft_putendl(str);
+	ft_strncat(str, buf, res);
+	ft_putendl("str + tmp  + buf : ");
+	ft_putendl(str);
 	free(tmp);
+	return (str);
 }
 
 static int	get_new_str(char *str)
@@ -56,23 +73,24 @@ int			get_next_line(int fd, char **line)
 {
 	char		buf[BUF_SIZE + 1];
 	static char	*strs[FD_NB];
-	char		*str;
+	//char		*str;
 	int			res;
 
 	if (fd < 0 || BUF_SIZE < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
-	if (!strs[fd] &&!(strs[fd] = ft_strnew(0)))
+	if (!strs[fd] && !(strs[fd] = ft_strnew(0)))
 		return (-1);
-	str = strs[fd];
+	//str = strs[fd];
 	res = 0;
-	while ((!ft_strchr(str, '\n')) && (res = read(fd, buf, BUF_SIZE)) > 0)
+	while ((!ft_strchr(strs[fd], '\n')) && (res = read(fd, buf, BUF_SIZE)) > 0)
 	{
-			buf[BUF_SIZE] = '\0';
-			get_new_buf(&str, buf, res);
+		buf[BUF_SIZE] = '\0';
+		strs[fd] = get_new_buf(strs[fd], buf, res);
 	}
 	if (res == -1)
 		return (-1);
-	*line = ft_strsub(str, 0, get_endl(str));
-	strs[fd] = str;
+	*line = ft_strsub(strs[fd], 0, get_endl(strs[fd]));
+	ft_putendl(*line);
+	//strs[fd] = str;
 	return (get_new_str(strs[fd]));
 }
--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: mbaron <mbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 18:31:04 by mbaron            #+#    #+#             */
-/*   Updated: 2018/01/27 14:47:11 by mbaron           ###   ########.fr       */
+/*   Updated: 2018/01/29 10:37:45 by mbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 int		put_error(char *str)
 {
-	ft_putendl((const char*) str);
+	ft_putendl(str);
 	return (1);
 }
 
@@ -55,18 +55,17 @@ int		main(int argc, char *argv[])
 		if (!(fd[i] = open(argv[i], O_RDONLY)))
 			return (put_error("File to read can't be opened !"));
 		output_file[i] = ft_strnew(ft_strlen(argv[i + 1]) + 8);
-		ft_strcpy(output_file[i], "output_");
-		ft_strcat(output_file[i], argv[i + 1]);
-		if (!(fd_output[i] = open(output_file[i], O_WRONLY)))
+		ft_strncpy(output_file[i], argv[i + 1], ft_strlen(argv[i + 1]) - ft_strlen(ft_strrchr(argv[i + 1], '/') + 1));
+		ft_strcat(output_file[i], "output_");
+		ft_strcat(output_file[i], ft_strrchr(argv[i + 1], '/') + 1);
+		if (!(fd_output[i] = open(output_file[i], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)))
 			return (put_error("File to write can't be opened !"));
 		gnl[i] = -1;
-		if (!(str[i] = ft_strnew(4096)))
+		if (!(str[i] = ft_strnew(BUF_SIZE)))
 			return (put_error("Malloc Error"));
-		ft_putstr("str ini : ");
-		ft_putendl(str[i]);
 		i++;
 	}
-	while(1)
+	while (1)
 	{
 		i = 0;
 		while (i < argc - 1)
@@ -87,10 +86,19 @@ int		main(int argc, char *argv[])
 				}
 				else if (gnl_exit(fd[i], fd_output[i], output_file[i], str[i]))
 					return (1);
+				exit(0);
+				/*
+				ft_putstr_fd("coucou", fd_output[i]);
+				ft_putstr("'");
+				ft_putstr("coucou");
+				ft_putstr("' is written in file : ");
+				ft_putendl(output_file[i]);
+				gnl_exit(fd[i], fd_output[i], output_file[i], str[i]);
+				gnl[i] = 0;
+				*/
 			}
 			i++;
 		}
-
 	}
 	return (0);
 }
